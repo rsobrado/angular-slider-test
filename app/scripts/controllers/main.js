6376dc795daf8f60ab297d61679015a203aa792f
Roger Sobrado <rsobrado@pzfullparty.com>
2015-03-23 19:47:52 -0600

    slider implementado

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,10 @@
  */
 angular.module('sliderTestApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.slides = [
+      	'images/example-slide-1.jpg',
+      	// 'images/example-slide-2.jpg',
+      	// 'images/example-slide-3.jpg',
+      	// 'images/example-slide-4.jpg'
     ];
   });
